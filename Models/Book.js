009fface326680c2325e7c88e205dafc9455548a
krishkumar4400 <krishkumarbgp2022@gmail.com>
2026-01-13 11:08:11 +0530

    created book schema

---
+++ Models/Book.js
@@ -0,0 +1,28 @@
+import mongoose, { Schema } from "mongoose";
+
+const bookSchema = new Schema({
+  title: {
+    type: String,
+    require: [true, "Book title is required"],
+    trim: true,
+    maxLength: [100, "Book title cannot be more than 100 characters"],
+  },
+  author: {
+    type: String,
+    require: [true, "Author name is required"],
+    trim: true,
+  },
+  year: {
+    type: Number,
+    require: [true, "Publication year is required"],
+    min: [1000, "Year must be atleast 1000"],
+    max: [new Date().getFullYear(), "Year cannot be in the future"],
+  },
+}, {
+    timestamps: true
+});
+
+
+const Book = mongoose.model.book || mongoose.model('book', bookSchema);
+
+export default Book;
